0246b022e57e7d600509a4ad4bf8863b593b15c0
Lsong <song940@gmail.com>
2021-08-11 14:24:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ import { ready } from 'https://lsong.org/scripts/dom.js';
 import { h, render, useState, useEffect } from 'https://unpkg.com/htm/preact/standalone.module.js';
 
 const App = () => {
+  const [ip, setAddress] = useState('');
+  useEffect(() => {
+    fetch('https://api.lsong.me/ip')
+      .then(res => res.text())
+      .then(setAddress)
+  });
   return [
     h('h2', null, "App"),
     h('ul', null, [
-      h('li', null, "Link")
+      h('li', null, ip)
     ])
   ]
 }
